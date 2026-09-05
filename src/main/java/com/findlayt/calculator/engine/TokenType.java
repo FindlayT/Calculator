dc27cd847616773feb7e746982ad4b37ce868443
FindlayT <80910092+FindlayT@users.noreply.github.com>
2026-09-05 20:40:09 +0100

    Created Token record and enum TokenType
    implemented a link through from controller -> engine -> tokeniser, does nothing yet

---
+++ src/main/java/com/findlayt/calculator/engine/TokenType.java
@@ -0,0 +1,5 @@
+package com.findlayt.calculator.engine;
+
+public enum TokenType {
+    NUMBER, PLUS, MINUS, MULTIPLY, DIVIDE
+}
---
+++ src/main/java/com/findlayt/calculator/engine/Tokeniser.java
@@ -0,0 +1,9 @@
+package com.findlayt.calculator.engine;
+
+import java.util.List;
+
+public class Tokeniser {
+    public static List<Token> tokenise(String input) {
+        return List.of();
+    }
+}
